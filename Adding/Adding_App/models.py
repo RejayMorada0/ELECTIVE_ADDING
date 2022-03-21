d79d7988d2e812cde6339a152d9cc7ead8074093
RejayMorada0 <rejaymorada0@gmail.com>
2022-03-21 11:57:28 +0800

    Add FIles

--- Adding/Adding_App/models.py
+++ Adding/Adding_App/models.py
@@ -3,3 +3,11 @@ import datetime
 import os
 
 # Create your models here.
+
+
+#All Subjects
+class All_Subject(models.Model):
+    sub_code = models.CharField(max_length=50)
+    sub_name = models.CharField(max_length=50)
+    year_and_sem = models.CharField(max_length=100)
+    offer_stats = models.CharField(max_length=100)
