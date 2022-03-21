7f5571415cec397161f98c79d75e10a63e129c78
CyrineOsorio <cyrineosorio18@gmail.com>
2022-03-21 16:26:02 +0800

    Add Files

--- Adding/Adding_App/models.py
+++ Adding/Adding_App/models.py
@@ -1,5 +1,5 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-import datetime
 import os
 
 # Create your models here.
@@ -8,8 +8,8 @@ import os
 class all_subjects(models.Model):
     sub_code = models.CharField(max_length=100, primary_key=True)
     sub_name = models.CharField(max_length=100)
-    year = models.PositiveIntegerField(max_length=100)
-    semester = models.PositiveIntegerField(max_length=100)
+    year = models.PositiveIntegerField()
+    semester = models.PositiveIntegerField()
     offer_stats = models.CharField(max_length=100)
 
 
@@ -36,28 +36,30 @@ class student_accounts(models.Model):
 
 #Student Request
 #foreign key (https://docs.djangoproject.com/en/4.0/topics/db/examples/many_to_one/)
+#https://docs.djangoproject.com/en/dev/ref/models/fields/#django.db.models.ForeignKey.related_name
+#https://stackoverflow.com/questions/2606194/django-error-message-add-a-related-name-argument-to-the-definition
 class student_request(models.Model): 
-    trans_id = models.PositiveIntegerField(max_length=100, primary_key=True)
-    stud_id = models.ForeignKey(student_accounts, on_delete=models.CASCADE)
-    sub_code = models.ForeignKey(all_subjects, on_delete=models.CASCADE)
-    sub_name = models.ForeignKey(all_subjects, on_delete=models.CASCADE)
-    year = models.ForeignKey(all_subjects, on_delete=models.CASCADE)
-    semester = models.ForeignKey(all_subjects, on_delete=models.CASCADE)
+    trans_id = models.PositiveIntegerField(primary_key=True)
+    stud_id = models.ForeignKey(student_accounts, on_delete=models.CASCADE, related_name='+')
+    sub_code = models.ForeignKey(all_subjects, on_delete=models.CASCADE, related_name='+')
+    sub_name = models.ForeignKey(all_subjects, on_delete=models.CASCADE, related_name='+')
+    year = models.ForeignKey(all_subjects, on_delete=models.CASCADE, related_name='+')
+    semester = models.ForeignKey(all_subjects, on_delete=models.CASCADE, related_name='+')
     remarks = models.CharField(max_length=100)
 
 
 #Head and PIC
 class head_access(models.Model): #add user type (admin or pic) one table
-    email = models.EmailField(max_length=100)
-    passw = models.CharField(max_length=100)
+    email1 = models.EmailField(max_length=100)
+    passw1 = models.CharField(max_length=100)
 
 #users model
 #https://simpleisbetterthancomplex.com/tutorial/2018/01/18/how-to-implement-multiple-user-types-with-django.html
-class User(AbstractUser):
-  USER_TYPE_CHOICES = (
-      (1, 'student'),
-      (2, 'pic'),
-      (3, 'head'),
-  )
+# class User(AbstractUser):
+#   USER_TYPE_CHOICES = (
+#       (1, 'student'),
+#       (2, 'pic'),
+#       (3, 'head'),
+#   )
 
-  user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
+#   user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
