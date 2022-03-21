d4b3df109481f5ae653151acc63d10c7f82234b4
RejayMorada0 <rejaymorada0@gmail.com>
2022-03-21 14:52:35 +0800

    Add Files

--- Adding/Adding_App/models.py
+++ Adding/Adding_App/models.py
@@ -4,7 +4,6 @@ import os
 
 # Create your models here.
 
-"""
 #All Subjects
 class all_subjects(models.Model):
     sub_code = models.CharField(max_length=100)
@@ -12,7 +11,38 @@ class all_subjects(models.Model):
     yr_and_sem = models.CharField(max_length=100)
     offer_stats = models.CharField(max_length=100)
 
-    class Meta:
-        db_table = "all_subjects"
-"""
 
+
+#Student Accounts
+class student_accounts(models.Model):
+    stud_id = models.CharField(max_length=100)
+    fn = models.CharField(max_length=100)
+    ln = models.CharField(max_length=100)
+    section = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    passw = models.CharField(max_length=100)
+    stud_stats = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+    
+
+#Student Request
+class student_request(models.Model):
+    trans_id = models.PositiveIntegerField(max_length=100)
+    stud_id = models.CharField(max_length=100)
+    sub_code = models.CharField(max_length=100)
+    sub_name = models.CharField(max_length=100)
+    yr_and_sem = models.CharField(max_length=100)
+    grades = models.CharField(max_length=100)
+    remarks = models.CharField(max_length=100)
+
+
+#Head Access
+class admin_access(models.Model):
+    email = models.EmailField(max_length=100)
+    passw = models.CharField(max_length=100)
+
+
+#PIC Access
+class admin_access(models.Model):
+    email = models.EmailField(max_length=100)
+    passw = models.CharField(max_length=100)
