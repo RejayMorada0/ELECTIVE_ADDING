2a7b1afe4ac8ca9d9930d6f72e6a2cb694723d2a
RejayMorada0 <rejaymorada0@gmail.com>
2022-03-22 14:54:47 +0800

    Add Files

--- Adding/Adding/urls.py
+++ Adding/Adding/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from Adding_App import views
 
 urlpatterns = [
     path('', include('Adding_App.urls')),
     path('admin/', admin.site.urls),
-    path('showAllSub', views.showAllSub, name='showAllSub'),
 ]
