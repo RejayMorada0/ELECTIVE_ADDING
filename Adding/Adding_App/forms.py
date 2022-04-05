63ed09e0b75bcf692a541e9c0429474b69b1c0d7
RejayMorada0 <rejaymorada0@gmail.com>
2022-04-05 10:34:27 +0800

    Add Files

--- Adding/Adding_App/forms.py
+++ Adding/Adding_App/forms.py
@@ -8,7 +8,7 @@ from .models import registration
 class StudentRegistration(UserCreationForm):
     class Meta:
         model = registration
-        fields = ['username', 'email', 'password1', 'password2', 'last_name', 'first_name', 'section', 'stud_id']
+        fields = ['username', 'email', 'password1', 'password2', 'last_name', 'first_name', 'section', 'stud_id', 'userType']
 
 
 class ReceiverRegistration(UserCreationForm):
